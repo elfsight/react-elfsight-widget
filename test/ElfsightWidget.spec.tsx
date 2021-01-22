1cc1f7f9a29a4152992e26ca0807a6515fef82b2
d521bb85 <d521bb85@gmail.com>
2021-01-22 21:07:43 +0300

    initial commit

---
+++ test/ElfsightWidget.spec.tsx
@@ -0,0 +1,7 @@
+import { ElfsightWidget } from '../src/ElfsightWidget';
+
+describe('ElfsightWidget', () => {
+  it('should be defined', () => {
+    expect(ElfsightWidget).toBeDefined();
+  });
+});
