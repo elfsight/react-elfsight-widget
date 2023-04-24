a6f00ba743929074a8b1a4c7ee6faff22097e00a
d521bb85 <d521bb85@gmail.com>
2023-04-24 18:11:32 +0300

    Update e2e-fixture app initialization

--- test/fixture/index.js
+++ test/fixture/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 
-render(<App />, document.querySelector('.app'));
+createRoot(document.querySelector('.app')).render(<App />);
